b57f45e35e9db7e8fd0b7c8d2225776b2d35edea
patrick aubin <patrick@capacitr.com>
2014-12-15 21:10:21 -0800

    Initial Commit

---
+++ PowerNote/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PowerNote
+//
+//  Created by paubins on 12/15/14.
+//  Copyright (c) 2014 paubins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PowerNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PowerNote
+//
+//  Created by paubins on 12/15/14.
+//  Copyright (c) 2014 paubins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
